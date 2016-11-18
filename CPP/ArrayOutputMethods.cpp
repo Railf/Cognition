39d5997cd21bab2345629ecbf90121769de31240
Ralph Wayne McCracken, III <Railf@users.noreply.github.com>
2016-11-18 11:55:37 -0600

    Create ArrayOutputMethods.cpp

---
+++ CPP/ArrayOutputMethods.cpp
@@ -0,0 +1,37 @@
+/*  
+    Array Output Methods
+        Array Arithmetic
+        
+    Ralph McCracken, III
+    November 18, 2016
+*/
+
+#include <iostream>
+
+const int arraySize = 5;          // Global const, integer variable, arraySize
+                                  // Initialized to 5 -- arbitrary
+
+int main()
+{
+	int ar[arraySize];              // Create integer array, ar, using arraySize variable (size of 5).
+	ar[3] = 7;                      // Assign the value of 7 to the array, location 3 (0 - 4).
+	
+	int *A = ar;                    // Create integer pointer, A, and assign integer array, ar, to it.
+	
+	                                // ================================================================
+	                                // = OUTPUT TO SCREEN                                             =
+	                                // ================================================================
+	                                // Different methods to output the value stored at arr[3].
+	                            
+	std::cout << ar[3]   << "\n";   // Display arr[3], 7; newline
+	
+	std::cout << *(A+3)  << "\n";   // Display dereferenced (A+3), 7; newline
+	
+	std::cout << *(ar+3) << "\n";   // Display dereferenced (ar+3), 7; newline
+}
+
+/* Outputs:
+            7
+            7
+            7
+*/
